280b839bcc008eaf26bc1a32321181eae6208411
sinha-eduard <77357095+sinha-eduard@users.noreply.github.com>
2024-09-25 15:40:34 -0400

    New Camp

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const Campground = require("./models/campground")
+const Campground = require("./models/campground");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Databse connected")
-})
-   
+  console.log("Databse connected");
+});
+
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, "views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-    res.render("home")
-})
+app.use(express.urlencoded({extended: true}))
 
-// app.get('/makecampground', async (req, res) => {
-//     const camp = new Campground({title: 'My Backyard', description:"Cheap camping"})
-//     await camp.save()
-//     res.send(camp)
-// })
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", async (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+    const camp = new Campground(req.body.campground);
+    await camp.save();
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
 
 app.listen(3000, () => {
-    console.log("PORT3000")
-})
+  console.log("PORT3000");
+});
--- seeds/index.js
+++ seeds/index.js
@@ -25,4 +25,6 @@ const seedDB = async () => {
     }
 }
 
-seedDB();
+seedDB().then(() =>{
+    mongoose.connection.close()
+})
