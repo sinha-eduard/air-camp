129c14a64df6f1be6030375e81cb393e9f88b979
sinha-eduard <77357095+sinha-eduard@users.noreply.github.com>
2024-10-03 20:29:58 -0400

    Started Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/views/**/*.ejs"],
+  content: [
+    "./public/views/**/*.ejs",
+    "./public/js/*.js"
+  ],
   theme: {
     extend: {},
   },
