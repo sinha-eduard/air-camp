ec30c1934430cb4cff096798b0a1dc62391aa14b
sinha-eduard <77357095+sinha-eduard@users.noreply.github.com>
2024-10-05 11:49:18 -0400

    More Styling

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate")
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp");
+mongoose.connect("mongodb://localhost:27017/air-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const Campground = require("../models/campground");
 const cities = require("./cities");
 const {places, descriptors} = require("./seedHelpers")
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect('mongodb://localhost:27017/air-camp')
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            price 
         })
         await camp.save();
     }
